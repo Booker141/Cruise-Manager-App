a4dbc44feab1344a8eafb693d44200ad202e361f
juancarlosld <56406256+juancarlosld@users.noreply.github.com>
2019-10-22 13:16:31 +0200

    Añadido clase cliente y barco

---
+++ cruceros/src/main/java/es/uca/iw/crusaito/cruceros/Cliente.java
@@ -0,0 +1,70 @@
+package es.uca.iw.crusaito.cruceros;
+
+import java.time.LocalDate;
+
+public class Cliente 
+{
+	private String cNombre;
+	private String cApellidos;
+	private String cDNI;
+	private int cTelefono;
+	private LocalDate cFchNac;
+	private String cDireccion;
+	private String cCiudad;
+
+	public String getcNombre() {
+		return cNombre;
+	}
+
+	public void setcNombre(String cNombre) {
+		this.cNombre = cNombre;
+	}
+
+	public String getcApellidos() {
+		return cApellidos;
+	}
+
+	public void setcApellidos(String cApellidos) {
+		this.cApellidos = cApellidos;
+	}
+
+	public String getcDNI() {
+		return cDNI;
+	}
+
+	public void setcDNI(String cDNI) {
+		this.cDNI = cDNI;
+	}
+
+	public int getcTelefono() {
+		return cTelefono;
+	}
+
+	public void setcTelefono(int cTelefono) {
+		this.cTelefono = cTelefono;
+	}
+
+	public LocalDate getcFchNac() {
+		return cFchNac;
+	}
+
+	public void setcFchNac(LocalDate cFchNac) {
+		this.cFchNac = cFchNac;
+	}
+
+	public String getcDireccion() {
+		return cDireccion;
+	}
+
+	public void setcDireccion(String cDireccion) {
+		this.cDireccion = cDireccion;
+	}
+
+	public String getcCiudad() {
+		return cCiudad;
+	}
+
+	public void setcCiudad(String cCiudad) {
+		this.cCiudad = cCiudad;
+	}
+}
