7fb17d01a3e87eb8db8b572636d9f3ccdf4e65d8
juancarlosld <56406256+juancarlosld@users.noreply.github.com>
2019-11-04 15:53:45 +0100

    Cambios en Vista Principal

--- cruceros/src/main/java/es/uca/gii/iw/crusaito/spring/MainView.java
+++ cruceros/src/main/java/es/uca/gii/iw/crusaito/spring/MainView.java
@@ -26,16 +26,7 @@ public class MainView extends VerticalLayout {
 
 	public MainView(@Autowired MessageBean bean) {
 		
-		Button toggleButton = new Button("Cambiar Tema", click -> {
-			ThemeList themeList = UI.getCurrent().getElement().getThemeList(); //
-
-			if (themeList.contains(Lumo.DARK)) { //
-				themeList.remove(Lumo.DARK);
-			} else {
-				themeList.add(Lumo.DARK);
-			}
-		});
-		add(toggleButton);
+		changeTheme(); // codigo reutilizable
 
 		TextField Tcodigo = new TextField("Código"); // esto para que?
 		add(Tcodigo);
@@ -48,35 +39,48 @@ public class MainView extends VerticalLayout {
 		
 		//Inicio body
 		
-		H1 titulo = new H1("Ofertas");	//Titulo
-		titulo.getStyle().set("margin-left", "40%");	//Titulo de la seccion izquierda
+		H1 titulo1 = new H1("Ofertas");	//Titulo
+		titulo1.getStyle().set("margin-left", "40%");	//Titulo de la seccion izquierda
 		
-		Image fotoPrueba = new Image("frontend/img/pruebaBarcoHeader.png","fotoOferta"); //Foto de cada barco, se debe extraer de la BD más adelante
-		fotoPrueba.setMaxHeight("350px");	//Altura foto
-		fotoPrueba.setMaxWidth("450px");	//Anchura foto
+		Image fotoPrueba1 = new Image("frontend/img/pruebaBarcoHeader.png","fotoOferta1"); //Foto de cada barco, se debe extraer de la BD más adelante
+		fotoPrueba1.setMaxHeight("350px");	//Altura foto
+		fotoPrueba1.setMaxWidth("450px");	//Anchura foto
 		
 		HorizontalLayout infoBarco1 = new HorizontalLayout(); //"Ficha" de cada oferta que consta de Foto, descripcion y precio.
-		Div texto = new Div();	//Texto con la descripcion
-		texto.add("Este barco parte desde Cádiz hasta Italia. "
+		Div texto1 = new Div();	//Texto con la descripcion
+		texto1.add("Este barco parte desde Cádiz hasta Italia. "
 				+ "Precio: 650€");
-		infoBarco1.add(fotoPrueba,texto);
+		infoBarco1.add(fotoPrueba1,texto1);
 		
-		HorizontalLayout infoBarco2 = new HorizontalLayout();	//Este infoBarco2 no aparece en la web
+		H1 titulo2 = new H1("Reservas");
+		titulo2.getStyle().set("margin-left", "40%");	//Titulo de la seccion derecha
 		
-		infoBarco2.add(fotoPrueba,texto);
+		Image fotoPrueba2 = new Image("frontend/img/pruebaBarcoHeader.png","fotoOferta2");
+		fotoPrueba2.setMaxHeight("350px");	//Altura foto
+		fotoPrueba2.setMaxWidth("450px");	//Anchura foto
+		
+		HorizontalLayout infoBarco2 = new HorizontalLayout();
+		Div texto2 = new Div();
+		texto2.add("Descripción de la reserva.");
+		infoBarco2.add(fotoPrueba2,texto2);
 
 		//Bloque izquierdo centro de la web
-		VerticalLayout leftVerticalBody = new VerticalLayout(titulo,infoBarco1,infoBarco2);
+		VerticalLayout leftVerticalBody = new VerticalLayout(titulo1, infoBarco1);
 		leftVerticalBody.getStyle().set("border-style", "solid");	//Bordes para comprobar limites
+		
 		//Bloque derecho centro de la web
-		VerticalLayout rightVerticalBody = new VerticalLayout();
+		VerticalLayout rightVerticalBody = new VerticalLayout(titulo2, infoBarco2);
 		rightVerticalBody.getStyle().set("border-style", "solid");	//Bordes para comprobar limites
+		
+		
 		//Bloque del cuerpo de la web que contiene los dos bloques verticales.
-		HorizontalLayout bodyHorizontal = new HorizontalLayout(leftVerticalBody,rightVerticalBody);
+		HorizontalLayout bodyHorizontal = new HorizontalLayout(leftVerticalBody, rightVerticalBody);
 		
 		bodyHorizontal.setWidthFull();
 		bodyHorizontal.getStyle().set("border-style", "solid"); //Bordes para comprobar limites.
+		
 		add(bodyHorizontal);
+		
 		//Fin body
 		
 		//Inicio Footer
@@ -85,4 +89,19 @@ public class MainView extends VerticalLayout {
 		
 		//Fin Footer
 	}
+	
+	public void changeTheme() {
+		
+		Button toggleButton = new Button("Cambiar Tema", click -> {
+			ThemeList themeList = UI.getCurrent().getElement().getThemeList(); //
+
+			if (themeList.contains(Lumo.DARK)) { //
+				themeList.remove(Lumo.DARK);
+			} else {
+				themeList.add(Lumo.DARK);
+			}
+		});
+		add(toggleButton);
+	}
+	
 }
