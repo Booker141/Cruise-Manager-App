e33ccaca115ba7bd1af34dd132642551e7478df8
manuelmdp <56405837+manuelmdp@users.noreply.github.com>
2019-11-14 16:26:16 +0100

    Cambio en UsuarioRepository.java
    Cambio en la herencia de UsuarioRepository de JpaRepositoryExtension a JpaRepository.

--- cruceros/src/main/java/es/uca/gii/iw/crusaito/spring/UsuarioRepository.java
+++ cruceros/src/main/java/es/uca/gii/iw/crusaito/spring/UsuarioRepository.java
@@ -1,14 +1,11 @@
 package es.uca.gii.iw.crusaito.spring;
 
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import es.uca.gii.iw.crusaito.clases.Usuario;
+//import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 
-public class UsuarioRepository extends JpaRepositoryExtension<Usuario, Long> {
-	Usuario findById(int id) {
-		
-	}
-	Usuario findByUsername(String username) {
-		
-	}
+//Sustituido JpaRepositoryExtension por JpaRepository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	Usuario findById(int id);
+	Usuario findByUsername(String username);
 }
